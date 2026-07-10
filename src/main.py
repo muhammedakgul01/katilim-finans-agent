7631b1994e1ea42bfc3edb05a66660b585d28cee
Muhammed Akgül <muhammedakgul01@gmail.com>
2026-07-10 12:39:03 +0300

    Export campaign data to JSON file

--- src/main.py
+++ src/main.py
@@ -1,8 +1,16 @@
 from extractor import vade_bul, kar_payi_bul, urun_bul, hedef_kitle_bul
 from normalizer import normalize_vade, normalize_kar_payi
+import json
+import os
 
 
 def main():
+    # main.py'nin bulunduğu klasör (src)
+    script_dizini = os.path.dirname(os.path.abspath(__file__))
+
+    # src'den bir üst dizine çık (proje_klasoru), sonra data/campaign.json'a in
+    dosya_yolu = os.path.join(script_dizini, "..", "data", "campaign.json")
+
     metin = "Yeni müşterilere özel %2,05 kâr payı ile 36 ay vadeli konut finansmanı."
 
     #Extractor ile bilgiler çıkarılır
@@ -26,6 +34,10 @@ def main():
     #Bilgiler yazdırılır
     print(kampanya)
 
+    kampanya_json = json.dumps(kampanya, indent = 4, ensure_ascii=False)
+
+    with open(dosya_yolu, "w", encoding="utf-8") as f:
+        f.write(kampanya_json)
 
 
 
